3570090a3444c6b3143f38a1da2a9e62a1c434c7
Ginger Allington <gallington.sesync.org>
2017-04-04 21:58:17 -0400

    commiting this version before I make a shit ton of changes to add in standardize vars

--- cfa.R
+++ cfa.R
@@ -1,5 +1,6 @@
 # See input.vars.R script first to format and tidy the data: 
-#
+# See standardizing.R script for code to standardize ecological variables
+# df w standardized vars is rpe.st
 
 # GROUPING"
 # ORDER EXOG?
@@ -7,7 +8,7 @@
 # INCORP 
 
 
-
+# 4 Apr: created new rpe.st dataframe. editing below to use this.
 
 # CFA test setup
 
@@ -52,7 +53,9 @@ pe.mod<- '
           ecol.ind =~ e1s + e2s + e3 + e4
           p2s ~~ p3s
           ecol.ind ~ practice 
-           '
+
+# BY ECOLOGICAL ZONE:
+'
 # change DS/ST/FMS accodingly:
 pe.fit.FMS <- sem(pe.mod, data = FMS,    #rpe,  
               std.lv = TRUE, 
@@ -100,7 +103,7 @@ cfa.model3 <- ' # measurement model
                 practice =~ pl + p2s + p3s + p4 + p5 + p6 +p7 +p8 +p9 + p10
                 ecol.ind =~ e1s + e2s + e3 + e4'
 # Note: have to specify which vars are ordered      
-ord<- c("r1", "r2")
+ord<- c("r1", "r2", "p4", "p5", "p6", "p7", "p8", "p9", "p10")
 rpe$r1<- ordered(rpe$r1, levels= c(0,1,2))
 rpe$r2<- ordered(rpe$r2, levels= c(0,1,2))
 
@@ -116,69 +119,57 @@ parameterEstimates(fit.cfa3)
 #
 # trying an sem w the rules vars added in:
 
-
-sem.test <- ' # measurement model  
+full.set <- '   
                 rules =~ r1 + r2
-                practice =~ pl + p2s + p3s + p4 + p5 + p6 +p7 +p8 +p9 #+ p10
-                ecol.ind =~ e1s + e2s + e3 + e4'
+                #practice =~ pl + p2s + p3s + p4 + p5 + p6 +p7 +p8 +p9 + p10
+                practice =~ p2s + p4 + p5 + p6 + p7   #subset the practices to those that were loading more
+                ecol.ind =~ e1s + e2s + e3 + e4
               # regressions
-                practice ~ rules'
-                ecol.ind ~ practice 
-                practice ~ ez'
-                #ecol.ind ~ rules + practice 
+                practice ~ rules
+                 ecol.ind ~ practice'
+                 practice ~ ez
+                ecol.ind ~ rules + practice 
                 e1s ~ ez
                 e2s ~ ez
-              # residual correlations
-                # r1 ~~ r2
-                # pl ~~ p2s
-                # pl ~~ p3s
-                 p2s ~~ p3s
-                # p6 ~~ p9
-                # p8 ~~ p10
-                # e1s ~~ e3
-                #e3 ~~ e4
-              '
-#not working  GETTING AN ERROR ASSOC W TYPES: FACTOR?????????
+                # residual correlations
+                r1 ~~ r2
+                pl ~~ p2s
+                pl ~~ p3s
+                p2s ~~ p3s
+                p6 ~~ p9
+                p8 ~~ p10
+                e1s ~~ e3
+                e3 ~~ e4
+                  '
+
+# comparing different model specifications:
+# sem v1
+#
+sem.test <- '   rules =~ r1 + r2
+                practice =~ p2s + p4 + p5 + p6 + p7   #subset the practices to those that were loading more
+                ecol.ind =~ e1s + e2s + e3 + e4
+              # regressions
+                ecol.ind ~ rules + practice 
+                #practice ~ rules
+                #ecol.ind ~ practice
+                '
+              
+# not working  GETTING AN ERROR ASSOC W TYPES: FACTOR?????????
+# SOLUTION : HAVE TO CALL ENDOG CATAGORICAL VARs ORDERED.
 fit.sem <- cfa(sem.test, data= rpe,
                std.lv = TRUE,
-               ordered = c("r1", "r2"))
-######################   
-
-
-
+               ordered = ord)
 summary(fit.sem)
-# semPaths(fit.sem,"std",edge.label.cex=0.5, curvePivot = TRUE)
-
-
-
-model <- ' # measurement model  
-rules =~ r1 + r2
-practice =~ pl + p2s + p3s + p4 
-ecol.ind =~ e1s + e2s + e3 + e4
-'
-### Command type 2: estimate parameters for the model
+semPaths(fit.sem,"std",edge.label.cex=0.5, curvePivot = TRUE, residuals=FALSE)
+semPaths(fit.sem)
+######################   
+# sem v2
+### 
 # have to specify which of the vars are ordered:
 
-# sem command is used
-model.est <- cfa(model, data=rpe, 
-                 ordered = c("r1", "r2", 
-                             "p4")) 
-
-### Command type 3: extract results from estimated model object
-summary(model.est, rsq=T) # summary of results
-summary(model.est, standardized=TRUE, rsq=TRUE)
-semPaths(model.est)
-semPaths(model.est,"std",edge.label.cex=0.5, curvePivot = TRUE)
-semPaths(model.est, "std", edge.label.cex = 0.5, curvePivot = TRUE, layout = "tree")
-parameterEstimates(model.est)
-standardizedSolution(model.est)
-
-
-
-mod2 <- ' # measurement model  
-                rules =~ r1 + r2
-                practice =~ pl + p2s + p3s + p7 + p10 #p4 + p5 + p6 +p7 +p8 +p9 + p10 #note this is PL not P1
-                ecol =~ e1s + e2s + e3 + e4
+sem2 <- ' rules =~ r1 + r2
+          practice =~ pl + p2s + p3s + p7 + p10 #p4 + p5 + p6 +p7 +p8 +p9 + p10 #note this is PL not P1
+          ecol =~ e1s + e2s + e3 + e4
           # regressions
           practice ~ rules
           ecol ~  practice
@@ -191,6 +182,35 @@ mod2 <- ' # measurement model
           e1s ~~ e3
           e3 ~~ e4
           '
+fit2 <- sem(sem2, data= rpe,
+               std.lv = TRUE,
+               ordered = ord)
+
+sem3 <- ' rules =~ r1 + r2
+          #practice =~ pl + p2s + p3s + p4 + p5 + p6 +p7 +p8 +p9 + p10
+          practice =~ p2s + p4 + p5 + p6 + p7   #subset the practices to those that were loading more
+          ecol.ind =~ e1s + e2s + e3 + e4
+          # regressions
+          practice ~ rules
+          ecol.ind ~ practice
+          practice ~ ez'
+      
+fit3 <- sem(sem3, data= rpe,
+               std.lv = TRUE,
+               ordered = ord)
+
+### Command type 3: extract results from estimated model object
+summary(model.est, rsq=T) # summary of results
+summary(model.est, standardized=TRUE, rsq=TRUE)
+semPaths(model.est)
+semPaths(model.est,"std",edge.label.cex=0.5, curvePivot = TRUE)
+semPaths(model.est, "std", edge.label.cex = 0.5, curvePivot = TRUE, layout = "tree")
+parameterEstimates(model.est)
+standardizedSolution(model.est)
+
+
+
+
 
 # sem command is used here
 model.est.2 <- sem(mod2, data=rpe,
