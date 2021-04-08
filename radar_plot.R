4ca0996c09cf519b1889761ad618ae099ad468ee
G. Allington <ginger.allington@gmail.com>
2021-04-08 12:23:09 -0400

    not using these anymore

--- radar_plot.R
+++ radar_plot.R
@@ -1,8 +1,126 @@
 
+
+
+
+# 4/1/2021: I don't think we want the radar plots.
+# It obscures the inherent predicted prob at control level.
+# while it's nice that they are all combined, it doesn't show the diffs as easily 
+# as the dot plots.
+
+
+
+
+
+#### SOLVED: THESE VALUES WERE ORIGINALLY COPIED AND PASTEDFROM THE
+# output that is pasted to console when you plot with
+# plotLMER.fnc()  which is in the predProb_each_by_ez.R script
+#   keeping those plotting scripts for now --what I used to generate the dor plots 
+# but I think what I really want to use
+# is the marginal effects predicted from 
+# get_model_data()  (no walso in that same script)
+# 
+
+
+
+
+# Look into this more.
+# Check get_model_data Help page
+# Think about what I really want to represent in the radar plots.
+# Is it the coefficients? bc that's not what I got, right?'
+# check for NAs
+map(mtcars, ~sum(is.na(.)))
+
+#extract marginal effects 
+  # if you use type = "eff" it holds the discrete predictors constant at their proportions
+wp.me <- get_model_data(hypw, type = "pred")
+
+  # this is the full model, though. not the divided out by ecological zone info...
+
+
+
+# pulled the predictions, but don't need them here really
+wp_pred<- as.data.frame(predict(hypw))
+names(wp_pred) <- c("wp_pred")
+
+# subset dataframe by ez ? 
+ds<- subset(td.fg, td.fg$ez==1)
+#st<- subset(to, to$ez==c(2,4)) # WHY ARE THESE COMBINED??
+st<- subset(td.fg, td.fg$ez ==2)
+es<- subset(td.fg, td.fg$ez==3)
+fms<- subset(td.fg, td.fg$ez==4)
+
+# played with how to use the original model to predict for a subset of the data. 
+# dwpred <- (predict(hypw, ds, allow.new.levels = TRUE)) 
+#names(dwpred) <- c("ds_wp")
+# dwpred$ds_wp <- as.numeric(dwpred$ds_wp)
+
+# March 27
+# Ok new plan, let's see if we calc the marg effects by indiv ez model?
+
+hypw1<- glmer(ResWint ~ hhTenureWPast + hhTenureWCamp + RuleYes + RuleFormal +  cogSC1 + frgCV + frg.left + (1 | Org), data = ds, family = binomial)
+
+
+# the predictions
+w1pr <- predict(hypw1)
+# the data used (obsv w/o NAs)
+df <- hypw1@frame
+dsw_me <- get_model_data(hypw1, type = "pred")
+
+# COMPARE THESE TO GOOGLE SHEETS? 
+
+
+Maybe I just plotted the betas in the radar plots? 
+Oh! Wait, I think maybe I used the z values.
+
+getME(hypw,"theta")
+fixef(hypw)
+
+# checking model convergence issues via 
+# suggestions from:
+# https://rstudio-pubs-static.s3.amazonaws.com/33653_57fc7b8e5d484c909b615d8633c01d51.html
+numcols <- grep("^c\\.",names(td.fg))
+dfs <- ds  # update dataset
+dfs[,numcols] <- scale(dfs[,numcols])
+hypw_sc <- update(hypw1,data=dfs)
+# ok
+
+# check singularity
+  # check that are not 0 or close to it
+tt <- getME(hypw_sc,"theta")
+ll <- getME(hypw_sc,"lower")
+min(tt[ll==0])
+# ok
+
+# scaled gradients
+derivs1 <- hypw_sc@optinfo$derivs
+sc_grad1 <- with(derivs1,solve(Hessian,gradient))
+max(abs(sc_grad1))
+max(pmin(abs(sc_grad1),abs(derivs1$gradient)))
+
+dd <- update(hypw_sc,devFunOnly=TRUE)
+pars <- unlist(getME(hypw_sc,c("theta","fixef")))
+grad2 <- grad(dd,pars)
+hess2 <- hessian(dd,pars)
+sc_grad2 <- solve(hess2,grad2)
+max(pmin(abs(sc_grad2),abs(grad2)))
+
+
+ss <- getME(hypw_sc,c("theta","fixef"))
+m2 <- update(hypw_sc,start=ss,control=glmerControl(optCtrl=list(maxfun=2e4)))
+
+  #MODEL CONVERGES IF CHANGE THE OPTIMIZER AND UP THE ITERATIONS
+m3 <- update(hypw_sc,start=ss,control=glmerControl(optimizer="bobyqa",
+                                                 optCtrl=list(maxfun=2e5)))
+
+
+
+
 # Load libraries ---------------- 
 library(fmsb) # for functiosn to plot radar charts
 library(cowplot)
-# Create dataframes ---------------
+library(sjPlot)
+
+
 
 # labels for groups
 pract = data.frame(
